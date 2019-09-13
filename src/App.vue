cc361132b7e576953df79853ecd7c1cafa93e1af
Alosies <alosies@gmail.com>
2019-09-13 23:04:26 +0530

    Stuck at paths

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="editor--container">
+  <div id="container">
   </div>
 </template>
 
@@ -35,27 +35,125 @@ export default class App extends Vue {
   public editor: monaco.editor.IStandaloneCodeEditor | null = null;
 
   private mounted() {
-    fetch('https://kubernetesjsonschema.dev/v1.14.0-standalone/pod-v1.json')
-      .then(res => res.json())
-      .then(resSchema => {
-        this.schema = resSchema;
-        console.log(this.schema);
-        const editorContainer: HTMLElement = document.getElementById(
-          'editor--container'
-        ) as HTMLElement;
+    this.addScripts();
+    this.configureEditor();
+    // fetch('https://kubernetesjsonschema.dev/v1.14.0-standalone/pod-v1.json')
+    //   .then(res => res.json())
+    //   .then(resSchema => {
+    //     this.schema = resSchema;
+    //     console.log(this.schema);
+    //     const editorContainer: HTMLElement = document.getElementById(
+    //       'editor--container'
+    //     ) as HTMLElement;
 
-        const service = new MonacoProviderService({
-          baseUrl: '',
-        });
+    //     const service = new MonacoProviderService({
+    //       baseUrl: '',
+    //     });
 
-        this.editor = service.create(editorContainer, {
-          model: monaco.editor.createModel(initialContent, 'yaml'),
-          language: 'yaml',
-          minimap: {
-            enabled: false,
+    //     this.editor = service.create(editorContainer, {
+    //       model: monaco.editor.createModel(initialContent, 'yaml'),
+    //       language: 'yaml',
+    //       minimap: {
+    //         enabled: false,
+    //       },
+    //     });
+    //     // this.addScripts();
+    //   });
+  }
+
+  private addScripts() {
+    const scripts: string[] = [
+      'lib/v0/vs/loader.js',
+      'lib/v0/vs/editor/editor.main.nls.js',
+      'lib/v0/vs/editor/editor.main.js',
+    ];
+
+    scripts.forEach(src => {
+      let script = document.createElement('script');
+      script.setAttribute('src', src);
+      document.head.appendChild(script);
+    });
+  }
+
+  configureEditor() {
+    let resolve;
+    const resPromise = new Promise(res => (resolve = res));
+    const paths = {
+      vs: './lib/v0/vs',
+    };
+
+    require.config({ // This seems to be the problem!!
+      paths: paths,
+    });
+
+    require([
+      'vs/basic-languages/monaco.contribution',
+      'vs/language/yaml/monaco.contribution',
+    ], () => {
+      this.editor = monaco.editor.create(document.getElementById('container'), {
+        value: '',
+        language: 'yaml',
+        automaticLayout: true,
+      });
+
+      this.editor.onDidChangeModelContent(() => {
+        // Following is a test of getting error markers
+        setTimeout(() => {
+          const markers = monaco.editor.getModelMarkers({});
+          console.log(markers);
+        }, 1000);
+      });
+
+      // See: https://github.com/Microsoft/vscode/blob/master/src/vs/editor/contrib/quickOpen/quickOpen.ts
+      require([
+        'vs/editor/contrib/quickOpen/quickOpen',
+      ], async quickOpen => {
+        const getSymbolsForPosition = (model, position) => {
+          return quickOpen.getDocumentSymbols(model).then(symbols => {
+            symbols = symbols.filter(symbol =>
+              symbol.range.containsPosition(position)
+            );
+            symbols = symbols.map(symbol => {
+              if (symbol.kind === 17) {
+                return `[]${symbol.name}`;
+              } else if (symbol.kind === 18 || symbol.kind === 1) {
+                return `{}${symbol.name}`;
+              } else {
+                return symbol.name;
+              }
+            });
+
+            return symbols;
+          });
+        };
+
+        monaco.languages.registerHoverProvider('yaml', {
+          provideHover: async (model, position) => {
+            const symbols = await getSymbolsForPosition(model, position);
+            return {
+              contents: [
+                {
+                  value: 'path: ' + symbols.join(' > '),
+                  isTrusted: true,
+                },
+              ],
+            };
           },
         });
+
+        // Breadcrumbs emulation:
+        this.editor.onDidChangeCursorSelection(async ({ selection }) => {
+          const model = this.editor.getModel();
+          const position = selection.getPosition();
+          const symbols = await getSymbolsForPosition(model, position);
+          // this.setState({ ...this.state, path: symbols.join(" > ") });
+        });
+
+        resolve();
       });
+    });
+
+    return resPromise;
   }
 }
 </script>
